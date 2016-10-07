a3c5a2039f75dd8fc6eb561a67bb44957f31772a
DennyGreb <dendendengrebenets@gmail.com>
2016-10-07 23:53:29 +0300

    Create first version of registration method

--- APP/views/views.py
+++ APP/views/views.py
@@ -1,11 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-import json
+import json, sys
 
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from models.users import User
+
+from APP.models import User
 
 def index(request):
     context = {
@@ -14,12 +15,20 @@ def index(request):
     return render(request, 'APP/index.html', context)
 
 
-def registraton(request):
+def registration(request):
 	if request.method == "POST":
 		post_data = json.load(request.body)
-		User.objects.create(full_name=post_data['full_name'],
-		                    email=post_data['email'], password=post_data['password'],
-		                    role_id=post_data['0'])
-	else:
-		print "Method doesn't allowed"
+		result_dict = dict()
+		if User.objects.filter(full_name=post_data['full_name']).exists():
+			result_dict['NameError'] = "Such full name already exists!"
+		if User.objects.filter(email=post_data['email']).exists():
+			result_dict['EmailError'] = "Such email already exists!"
+		if User.objects.filter(password=post_data['password']).exists():
+			result_dict['PassError'] = "Such password already exists!"
+		if not result_dict:
+			User.objects.create(full_name=post_data['full_name'],
+		    	                email=post_data['email'], password=post_data['password'],
+		        	            role_id=post_data['0'])
+			result_dict['Success'] = "Your registration has been succesfully done"
+		return JsonResponse(result_dict)
 		
