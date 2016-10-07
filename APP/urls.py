524a891f34ab2676c93bfcbb780583bf0ce0a7d3
DennyGreb <dendendengrebenets@gmail.com>
2016-10-07 20:53:03 +0300

    Start to develop registration

--- APP/urls.py
+++ APP/urls.py
@@ -4,4 +4,5 @@ from .views import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^registration', views.registration)
 ]
--- APP/views/views.py
+++ APP/views/views.py
@@ -1,9 +1,25 @@
 # -*- encoding: utf-8 -*-
 
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from models.users import User
 
 def index(request):
     context = {
         'message': "Привіт, світе! Ви на головній сторінці Рівненського велододатку.",
         }
     return render(request, 'APP/index.html', context)
+
+
+def registraton(request):
+	if request.method == "POST":
+		post_data = json.load(request.body)
+		User.objects.create(full_name=post_data['full_name'],
+		                    email=post_data['email'], password=post_data['password'],
+		                    role_id=post_data['0'])
+	else:
+		print "Method doesn't allowed"
+		
