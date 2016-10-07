0c0ecf5e8d7b4d22849b29df9930c6318b3c3111
Olexii.Lykianchyk <olexii.lykianchyk@gmail.com>
2016-10-07 16:46:19 +0300

    done password hashing and check password methods

--- APP/views/views.py
+++ APP/views/views.py
@@ -7,3 +7,10 @@ def index(request):
         'message': "Привіт, світе! Ви на головній сторінці Рівненського велододатку.",
         }
     return render(request, 'APP/index.html', context)
+
+
+@require_http_methods(['POST'])
+def login(request):
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    
