106ffdf13edf2804f22891df140b52ed49cf96f1
DennyGreb <dendendengrebenets@gmail.com>
2016-10-11 18:59:35 +0300

    Modify registation jsx

--- static/jsx/registration.jsx
+++ static/jsx/registration.jsx
@@ -1,3 +1,4 @@
+var dict = []
 class FormComponent extends React.Component {
     render() {
         return (
@@ -27,18 +28,26 @@ class Legend extends React.Component {
 
 class UserName extends React.Component {
     constructor(props){
-        super(props);
+    super(props);    
+    this.state = {full_name: 'Your beautiful full name here!'};
+    this.handleChange = this.handleChange.bind(this);
     }
-    handleEmailChange(e) {
-        this.setState({full_name: e.target.value});
+
+    handleChange(event) {
+        this.setState({full_name: event.target.value});
+        dict.push({
+            full_name: event.target.value
+        })
+        console.log(dict);
     }
+
     render() {
         return(
         <div className="control-group">
             <label className="control-label" htmlFor="username">Username</label>
             <div className="controls">
-                <input type="text" id="username" name="full_name" placeholder=""
-                 className="input-xlarge" onChange={this.state.full_name}/>
+                <input type="text" id="username" name="full_name"
+                 className="input-xlarge" value={this.state.full_name} onChange={this.handleChange}/>
                 <p className="help-block">{this.props.name}</p>
             </div>
         </div>
@@ -48,15 +57,23 @@ class UserName extends React.Component {
 
 
 class Email extends React.Component {
-    constructor(props){
-        super(props);
+    constructor(props) {
+    super(props);    
+    this.state = {email: 'Your perfect email here!'};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({email: event.target.value});
+        console.log(this.state)
     }
     render() {
         return (
         <div className="control-group">
             <label className="control-label" htmlFor="email"></label>
             <div className="controls">
-                <input type="text" id="email" name="email" placeholder="" className="input-xlarge" />
+                <input type="text" id="email" name="email" 
+                className="input-xlarge" value={this.state.email} onChange={this.handleChange}/>
                 <p className="help-block">{this.props.name}</p>
             </div>
         </div>
@@ -65,15 +82,23 @@ class Email extends React.Component {
 }
 
 class Pass extends React.Component {
-    constructor(props){
-        super(props);
+    constructor(props) {
+    super(props);    
+    this.state = {password: ''};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({password: event.target.value});
+        console.log(this.state)
     }
     render() {
         return (
         <div className="control-group">
             <label className="control-label" htmlFor="password"></label>
             <div className="controls">
-                <input type="password" id="password" name="password" placeholder="" className="input-xlarge" />
+                <input type="password" id="password" name="password" className="input-xlarge" 
+                value={this.state.password} onChange={this.handleChange}/>
                 <p className="help-block">{this.props.name}</p>
             </div>
         </div>
@@ -82,15 +107,23 @@ class Pass extends React.Component {
 }
 
 class PassConfirm extends React.Component {
-    constructor(props){
-        super(props);
+    constructor(props) {
+    super(props);    
+    this.state = {password_confirm: ''};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({password_confirm: event.target.value});
+        console.log(this.state)
     }
     render() {
         return (
         <div className="control-group">
             <label className="control-label"  htmlFor="password_confirm"></label>
             <div className="controls">
-                <input type="password" id="password_confirm" name="password_confirm" placeholder="" className="input-xlarge" />
+                <input type="password" id="password_confirm" name="password_confirm" className="input-xlarge" 
+                value={this.state.password_confirm} onChange={this.handleChange}/>
                 <p className="help-block">{this.props.name}</p>
             </div>
         </div>
@@ -99,11 +132,20 @@ class PassConfirm extends React.Component {
 }
 
 class SuccessButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {formValues: []};
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
         <div className="control-group">
             <div className="controls">
-                <button className="btn btn-success">Register</button>
+                <button className="btn btn-success" onClick={this.handleClick}>Register</button>
             </div>
         </div>
         );
