3162b88da93d0625d2aa1e9b527e0dfd698a7153
DennyGreb <dendendengrebenets@gmail.com>
2016-11-03 18:09:46 +0200

    Make marker details page

--- static/jsx/marker_detail_page.jsx
+++ static/jsx/marker_detail_page.jsx
@@ -28,6 +28,54 @@ export class MarkerDetails extends React.Component {
 		});
 	}
 
+	renderPlaceCondition(){
+		if(this.state.marker_type === "Place"){
+			return(
+				<div className="card info">
+					<h3 className="detail-cards-header">Info</h3>
+					<span>Works from {this.state.marker_value.from_hour} to {this.state.marker_value.to_hour}</span>
+				</div>
+			)
+		}
+	}
+
+	renderBikeCondition(){
+		if(this.state.marker_type === "StolenBike"){
+			return(
+				<div className="card info">
+					<h3 className="detail-cards-header">Info</h3>
+					<span>This bike was stolen at {this.state.marker_value.day}</span>
+					<span>{this.state.marker_value.is_found ? "This bike was already found" : "This bike still lost"}</span>
+				</div>
+			)
+		}
+	}
+
+	renderParkingCondition(){
+		if(this.state.marker_type === "Parking"){
+			return(
+				<div className="card info">
+					<h3 className="detail-cards-header">Info</h3>
+					<span>Amount of this parking is {this.state.marker_value.amount}</span>
+					<span>{parseInt(this.state.marker_value.security) ? "This parking is secure" : "This parking isn't secure"}</span>
+				</div>
+			)
+		}
+	}
+
+	renderDetails(){
+		if(this.state.marker_type !== "Parking"){
+			return (
+				<div className="col-xs-12 col-md-6 col-lg-6">
+					<div className="card details">
+						<h3 className="detail-cards-header">Description</h3>
+						<p>{this.state.marker_value.description}</p>
+					</div>
+				</div>
+			);
+		}	
+	}
+
 	render() {
 		console.log(this.state.marker_value);
 		return (
@@ -38,23 +86,18 @@ export class MarkerDetails extends React.Component {
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-xs-12 col-md-6 col-lg-6">
-						<div className="card details">
-							<h3 className="detail-cards-header">Description</h3>
-							<p>{this.state.marker_value.description}</p>
-						</div>
-					</div>
+					{ this.renderDetails() }
 					<div className="col-xs-12 col-md-6 col-lg-6">
 						<div className="card location">
 							<h3 className="detail-cards-header">Location</h3>
-							<span className="material-icons">place</span>
-							<span>location...</span>
+							<span className="location-icon material-icons">place</span>
+							<span>Coordinates lan: {this.state.marker_value.lng} lat: {this.state.marker_value.lat}</span>
 						</div>
 					</div>
 					<div className="col-xs-12 col-md-6 col-lg-6">
-						<div className="card info">
-							<h3 className="detail-cards-header">Info</h3>
-						</div>
+							{ this.renderPlaceCondition() }
+							{ this.renderBikeCondition() }
+							{ this.renderParkingCondition() }
 					</div>
 				</div>
 			</div>
