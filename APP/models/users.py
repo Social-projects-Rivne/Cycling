f703b7f9869feb7d7eed95f5c78bd367d51e0f21
Dmytro <tsukanovdmytro@gmail.com>
2016-11-18 14:52:31 +0200

    1

--- APP/models/users.py
+++ APP/models/users.py
@@ -1,11 +1,19 @@
 from __future__ import unicode_literals
+from random import choice
 
 from django.db import models
 
-# Create your models here.
+av_list = [
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217538/default-avatar-ponsy-deer.png',
+    'http://images.all-free-download.com/images/graphiclarge/vector_pleasant_goat_avatar_179848.jpg',
+    'http://s3-us-west-1.amazonaws.com/witty-avatars/default-avatar-3-l.jpg',
+    'https://tiltify.com/assets/default-avatar-0b86a09c31209b5eb42080feeafc4dd4.jpg',
+    'http://i.imgur.com/vDhhizN.jpg?1',
+]
 
 
 class User(models.Model):
+    """"""
 
     ROLES = (
         ('0', 'user'),
@@ -16,7 +24,7 @@ class User(models.Model):
     email = models.EmailField(unique=True, db_index=True)
     password = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    avatar = models.URLField(blank=True, default="http://placehold.it/250x250")
+    avatar = models.URLField(blank=True, default=choice(av_list))
     role_id = models.CharField(max_length=1, choices=ROLES, default='0')
     token = models.CharField(max_length=255, db_index=True, blank=True, default=None)
 
