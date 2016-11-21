e84f54f47c0c1e5b7aae59721a84268222570a3f
Dmytro <tsukanovdmytro@gmail.com>
2016-11-21 15:06:14 +0200

    Converting LESS into CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var webpack = require('webpack-stream');
+var less = require('gulp-less');
 
 var path = {
   HTML: 'APP/templates/APP/index.html',
-  ALL: ['static/css/*.css', 'static/jsx/**/*.js', 'static/jsx/*.jsx', 'static/jsx/**/*.jsx'],
+  ALL: ['static/jsx/*.jsx', 'static/jsx/**/*.jsx'],
   JSX: ['static/jsx/*.jsx', 'static/jsx/**/*.jsx'],
   MINIFIED_OUT: 'bundle.js',
   DEST_BUILD: 'static/js/app',
+  LESS: 'static/less/*.less',
+  CSS: 'static/css',
 };
 
 function swallowError (error) {
@@ -18,6 +21,14 @@ function swallowError (error) {
 
   this.emit('end')
 }
+
+// Compiles LESS > CSS 
+gulp.task('less', function(){
+    return gulp.src(path.LESS)
+        .pipe(less())
+        .on('error', swallowError)
+        .pipe(gulp.dest(path.CSS));
+});
 
 gulp.task('transform', function() {
   return gulp.src(path.JSX)
@@ -37,6 +48,7 @@ gulp.task('build', function(){
 
 gulp.task('watch', function(){
   gulp.watch(path.ALL, ['transform']);
+  gulp.watch(path.LESS, ['less']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['transform', 'watch']);
