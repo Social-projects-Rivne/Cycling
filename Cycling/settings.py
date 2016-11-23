a138d481f266752c91503667d60eb01a39d28818
Dmytro <tsukanovdmytro@gmail.com>
2016-11-23 21:22:23 +0200

    1

--- Cycling/settings.py
+++ Cycling/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = '+#alll+=%$)3(0@yy*tgf264m)h-lx*sdytyst#%c$=-yf8u&o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = [i[1] for i in config.items('ALLOWED_HOSTS')]
 
 # Application definition
 
@@ -81,10 +80,11 @@ DB_PASSWORD = config.get('DataBase', 'PASSWORD')
 DB_HOST = config.get('DataBase', 'HOST')
 DB_PORT = config.get('DataBase', 'PORT')
 DB_NAME = config.get('DataBase', 'NAME')
+DB_ENGINE = config.get('DataBase', 'ENGINE')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.%s' % DB_ENGINE,
         'HOST': DB_HOST,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
@@ -130,11 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = config.get('Site', 'STATIC_URL')
 
 STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')),
                     (os.path.join(BASE_DIR, 'node_modules')),
-                    )
-
-#                [latitude , longitude]
-RIVNE_LOCATION = [50.619776, 26.251265]
+                   )
