f1cdbae280b120ca7d8a206f00561204a86321f6
Dmytro <tsukanovdmytro@gmail.com>
2016-11-28 12:52:01 +0200

    1

--- static/jsx/profile.jsx
+++ static/jsx/profile.jsx
@@ -250,7 +250,7 @@ let editBikeButton = function(bike){
         return null;
     };
     return (
-        <span id="location-icon" className="material-icons pencil">
+        <span className="material-icons pencil">
             <Link type="button" className="btn btn-default btn-xs" to={'/bike/' + bike.id}>edit</Link>
         </span>
     );
